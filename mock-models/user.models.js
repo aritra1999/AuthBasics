0ecb933fb0c010823c664d020c51c1b9258891f9
Aritra Mondal <mondalaritra002@gmail.com>
2022-10-10 17:17:19 +0530

    Added mock user data

---
+++ mock-models/user.models.js
@@ -0,0 +1,12 @@
+export default users = [
+    {
+        'name': 'Hello World', 
+        'username': 'hello',
+        'email': 'hello@world.com',
+    }, 
+    {
+        'name': 'Jhon Doe', 
+        'username': 'jhon',
+        'email': 'jhon@doe.com',
+    }
+]
